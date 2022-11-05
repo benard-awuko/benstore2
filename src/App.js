9b99a4181f5092631dff0164da8592c84f5eb1ec
Benard Awuko <benawuko@gmail.com>
2022-11-05 21:52:21 +0300

    Pages Created and react router dom installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import {About, Error, Home, Products, SingleProduct} from './pages'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+       <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='about' element={<About></About>}>Home</Route>
+          <Route path='products' element={<Products></Products>}>Home</Route>
+          <Route path='products/:{id}' element={<SingleProduct></SingleProduct>}>Home</Route>
+          <Route path='*' element={<Error></Error>}>Error</Route>
+       </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
